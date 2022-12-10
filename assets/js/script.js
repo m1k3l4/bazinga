222d018040479346e171692534000f9a8855f564
m1k3l4 <micheladalema@gmail.com>
2022-12-10 15:05:26 +0000

    Add script structure and placeholders for final code

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+//global variables
+
+//function for opponent random choice
+function randomChoice() {
+
+}
+
+//function to evaluate and generate result
+function resultEvaluation() {
+
+}
+
+//function to increment score value
+function scoreValue() {
+
+}
+
+//main code to get player choice and invoke functions
