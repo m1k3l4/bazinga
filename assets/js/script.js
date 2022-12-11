eb26d68f89c0ef26244ec52008a6af137d196026
m1k3l4 <micheladalema@gmail.com>
2022-12-11 13:42:54 +0000

    Add random choice function body for Leonard, and players selection display span elements styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,25 @@ let sheldon;
 let leonard;
 
 //function for opponent random choice
-function randomChoice() {
+function leonardChoice() {
+    let randomValue = Math.floor(Math.random() * 5) + 1;
 
+    if (randomValue === 1) {
+        leonard = 'rock';
+    } else if (randomValue === 2) {
+        leonard = 'paper';
+    } else if (randomValue === 3) {
+        leonard = 'scissors';
+    } else if (randomValue === 4) {
+        leonard = 'lizard';
+    } else {
+        leonard = 'spock';
+    }
+    displayLeonardChoice.innerHTML = leonard;
 }
+
+leonardChoice();
+
 
 //function to evaluate and generate result
 function resultEvaluation() {
