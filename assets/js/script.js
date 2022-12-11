3c7f989c4017586aadf5e8cb6e4f1410b9452fb4
m1k3l4 <micheladalema@gmail.com>
2022-12-11 13:19:50 +0000

    Add global variables and constants

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
-//global variables
+//global variables and constants
+const allOptions = document.querySelectorAll("button");
+const displaySheldonChoice = document.getElementById('player_sheldon');
+const displayLeonardChoice = document.getElementById('player_leonard');
+const sheldonScore = document.getElementById('sheldon_score');
+const leonardScore = document.getElementById('leonard_score');
+const displayResult = document.getElementById('outcome');
+let globalSheldonScore = 0;
+let globalLeonardScore = 0;
+let gameResult;
+let sheldon;
+let leonard;
 
 //function for opponent random choice
 function randomChoice() {
